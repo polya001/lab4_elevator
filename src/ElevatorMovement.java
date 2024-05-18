f970d9d940c3ab6bda589e74ebc97d785d2b4eed
polya001 <99148202+polya001@users.noreply.github.com>
2024-05-19 00:19:20 +0300

    Update ElevatorMovement.java

--- src/ElevatorMovement.java
+++ src/ElevatorMovement.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class ElevatorMovement {
+import java.util.*;
+
+/**
+
+ * Flow class for managing elevators movement
+ */
+public class ElevatorMovement implements Runnable{
+
+    ElevatorControl Control;
+    ElevatorMovement(ElevatorControl control){
+        Control=control;
+    }
+    @Override
+    public void run() {
+        while (!(Control.el1.SetStops.isEmpty()) && !(Control.el2.SetStops.isEmpty())){
+
+            Control.Movement();
+            try {
+                Thread.sleep(1100);            //передвижение на 1 этаж
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
 }
