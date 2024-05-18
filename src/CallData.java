89353b87e6ed02917a922bfdf1382926077d930c
polya001 <99148202+polya001@users.noreply.github.com>
2024-05-19 00:20:22 +0300

    Update CallData.java

--- src/CallData.java
+++ src/CallData.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class CallData {
+public class CallData {
+    int StartFloor,FinalFloor;
+    int Direction;  //-1 - down, 1 - up
+    CallData(int startFloor,int finalFloor, int direction){
+        StartFloor = startFloor;
+        FinalFloor = finalFloor;
+        Direction=direction;
+    }
 }
