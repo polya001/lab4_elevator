0ca84726e91b9feb453d7a5e12e528fd22f926f7
polya001 <99148202+polya001@users.noreply.github.com>
2024-05-19 00:17:58 +0300

    Update Elevator.java

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Elevator {
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Elevator {
+    int CurrentFloor,FinalFloor;  //FinalFloor - макс. этаж по текущему напр.,
+                                                  
+    int Direction;  //-1 - down, 1 - up, 0 - stands still
+
+    ArrayDeque<Integer> SetStops;
+    ArrayDeque<Integer> FloorsExit;
+
+    Elevator(){
+        CurrentFloor=0;
+        Direction=0;
+        FinalFloor = 0;
+        SetStops = new ArrayDeque<>();
+        FloorsExit= new ArrayDeque<>();
+
+    }
+
 }
