d53225031051bee93b8154faf15302909826a7e3
polya001 <99148202+polya001@users.noreply.github.com>
2024-05-19 00:20:42 +0300

    Update PassagerCalls.java

--- src/PassagerCalls.java
+++ src/PassagerCalls.java
@@ -1,23 +1,43 @@
 import java.util.*;
 
-public class ElevatorCalls implements Runnable {
+/**
 
-    public void Calls(int Count){
-        Scanner scanner = new Scanner(System.in);
-        for (int i=0;i<Count;i++){
-            System.out.println("Enter number of floor");
-            int number = scanner.nextInt();
-            System.out.println("Enter direction: 1 (if up) or -1 (if down)");
-            int dir = scanner.nextInt();
+ * Flow class for managing elevator calls
+ */
+public class PassagerCalls implements Runnable {
 
+    ElevatorControl control;
+    int CountCalls;
+    PassagerCalls(int count_calls,ElevatorControl elevatorControl){
+        CountCalls=count_calls;
+        control=elevatorControl;
 
-        }
 
     }
 
 
     @Override
     public void run() {
+
+        Scanner scanner = new Scanner(System.in);
+        for (int i=0;i<CountCalls;i++){
+            System.out.println("Enter number of current floor");
+            int start= scanner.nextInt();
+            System.out.println("Enter number of final floor");
+            int end = scanner.nextInt();
+
+            int dir = start>end ? -1:1;
+            control.FixCalls(new CallData(start,end,dir));
+
+
+            try {
+                Thread.sleep(1000);        //время между запросами
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
 
     }
 }
