3a6aa31c87611fd7a6f0e6f72edc9b231fd9ef93
polya001 <99148202+polya001@users.noreply.github.com>
2024-05-18 23:58:27 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import java.util.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Enter count of requests");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+        int count_calls = scanner.nextInt();
+        System.out.println("Enter count of floors");
+        int count_floors =scanner.nextInt();
+        ElevatorControl newControl = new ElevatorControl(count_floors);
+        Thread calls = new Thread(new PassagerCalls(count_calls,newControl));
+        Thread elevatorMovement = new Thread(new ElevatorMovement(newControl));
+        calls.start();
+        elevatorMovement.start();
+        calls.join();
+        elevatorMovement.join();
+
+        
+
+
     }
 }
